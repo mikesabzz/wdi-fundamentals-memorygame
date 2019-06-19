4c70355d0ec5ad5bd89f60be2f1e502c622a923d
Michael Sabzevari <michaelsabzevari@Michaels-MacBook-Air.local>
2019-06-19 16:44:07 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -5,4 +5,13 @@ var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "king";
 
-console.log("User flipped " + cardOne);
+
+var flipCard = cardID;
+console.log("User flipped" + cards[cardID]);
+
+var checkForMatch = cardID;
+if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+}
